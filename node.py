65ecd081b60d554be347af25d33067cf14a188db
aarushi24 <aarushisarbhai16@gmail.com>
2018-09-17 12:49:45 -0600

    Add comments

--- node.py
+++ node.py
@@ -5,54 +5,54 @@ import flow
 class Node:
 	def __init__(self, env):
 		self.env = env
-		self.parent_rp = []
-		self.child_rp = []
-		self.cspace = [] # Flows that it can send on
-		self.listener = [] # Flows it is listening on
+		self.parent_rp = [] # all Rendezvous Points that the node is connected to
+		self.child_rp = [] # all Rendezvous Points that are created/owned by the node
+		self.cspace = [] # all Flows the node can send on
+		self.listener = [] # all Flows the node is listening on
 		#self.blocker = False
 	
 	def createNode(self, new_node):
-		new_rp = rendezvousPoint.RendezvousPoint(self.env, self)
-		yield new_rp.rp.request()
-		self.env.process(new_node.addParent(new_rp))
-		new_rp.attachNode(new_node)
+		new_rp = rendezvousPoint.RendezvousPoint(self.env, self) 
+		yield new_rp.rp.request() # Request for parent node to acquire one end of the RP
+		self.env.process(new_node.addParent(new_rp)) # Schedule the RP assignment to new node
 		self.child_rp.append(new_rp)
 		#print(self.child_rp)
 	
 	def addParent(self, new_rp):
-		self.parent_rp.append(new_rp)
-		yield new_rp.rp.request()
+		self.parent_rp.append(new_rp) 
+		yield new_rp.rp.request() # Request for child node to acquire other end of the RP
+		new_rp.attachNode(self) # Add new node as the other end of the RP
 		#print(self.parent_rp)
 
 	def createFlow(self, flow_specs): 
-		new_flow = flow.Flow(self.env, flow_specs)
-		yield new_flow.flow.request()
-		self.cspace.append(new_flow)
+		new_flow = flow.Flow(self.env, flow_specs) 
+		yield new_flow.flow.request() # Flow requested by the node
+		self.cspace.append(new_flow) # Add flow to CSpace of the node
 		#print(new_flow)
 
 	def createRendezvousPoint(self):
 		new_rp = rendezvousPoint.RendezvousPoint(self.env, self)
-		yield new_rp.rp.request()
+		yield new_rp.rp.request() # Request for parent node to acquire one end of the RP
 		self.parent_rp.append(new_rp)
 		#print(new_rp)
 
 	def sendCap(self):
                 #yield self.env.timeout(1)
-                self.cspace[0].put('test')
-                self.env.process(self.recvCap())
+                self.cspace[0].put('test') # Send message on flow 
+                self.env.process(self.recvCap()) # schedule receive
 
         def recvCap(self):
-                msg = yield self.cspace[0].get()
+                msg = yield self.cspace[0].get() # Get value from flow
                 print(msg)
 	
 	def revoke(self):
+		# Remove all flows in cspace of the node
 		for c in self.cspace:
-			if c.type == "flow":
-				c.callback()
+			c.callback() 
 
 	def delete(self, c):
-		if c.type == "flow":
-			c.callback()
+		# Remove specific flow from cspace
+		c.callback()
 
 	#def revoke(self):
 	#	for c in cspace:
