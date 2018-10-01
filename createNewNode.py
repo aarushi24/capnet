1167d6af0e1b063c892929dd00457221bc226160
aarushi24 <aarushisarbhai16@gmail.com>
2018-10-01 15:14:59 -0600

    Testing scripts

--- createNewNode.py
+++ createNewNode.py
@@ -7,6 +7,7 @@ env = simpy.Environment()
 n1 = node.Node(env)
 print(n1)
 n2 = node.Node(env)
+env.process(n1.createMembrane())
 env.process(n1.createNode(n2))
 print(n2)
 
--- createType.py
+++ createType.py
@@ -6,6 +6,7 @@ env = simpy.Environment()
 #init events
 n1 = node.Node(env)
 print(n1)
+env.process(n1.createMembrane())
 env.process(n1.createRendezvousPoint())
 env.process(n1.createFlow(None))
 
